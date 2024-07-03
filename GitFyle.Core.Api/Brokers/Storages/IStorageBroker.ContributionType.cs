cfbf841d1fc4c4aa97ecd2ec6a967b88fd24810a
Lezune <jakeleque@gmail.com>
2024-07-03 01:23:21 -0700

    Broker: SelectContributionTypeByIdAsync - Method Added to StorageBroker.

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.ContributionType.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.ContributionType.cs
@@ -1,7 +1,11 @@
-﻿namespace GitFyle.Core.Api.Brokers.Storages
+﻿using GitFyle.Core.Api.Models.Foundations.ContributionTypes;
+using System;
+using System.Threading.Tasks;
+
+namespace GitFyle.Core.Api.Brokers.Storages
 {
     internal partial interface IStorageBroker
     {
-
+        ValueTask<ContributionType> SelectContributionTypeByIdAsync(Guid contributionTypeId);
     }
 }
--- GitFyle.Core.Api/Brokers/Storages/StorageBroker.ContributionTypes.cs
+++ GitFyle.Core.Api/Brokers/Storages/StorageBroker.ContributionTypes.cs
@@ -4,11 +4,16 @@
 
 using GitFyle.Core.Api.Models.Foundations.ContributionTypes;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
 
 namespace GitFyle.Core.Api.Brokers.Storages
 {
     internal partial class StorageBroker
     {
         public DbSet<ContributionType> ContributionTypes { get; set; }
+
+        public async ValueTask<ContributionType> SelectContributionTypeByIdAsync(Guid contributionTypeId) =>
+            await SelectAsync<ContributionType>(contributionTypeId);
     }
 }
