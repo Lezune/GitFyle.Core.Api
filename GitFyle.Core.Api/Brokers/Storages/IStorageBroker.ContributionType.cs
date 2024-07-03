be3732e2cfd155453605f7b519ce9619f58d96da
Lezune <jakeleque@gmail.com>
2024-07-03 01:28:04 -0700

    Broker: DeleteContributionTypeAsync - Method Added to StorageBroker.

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.ContributionType.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.ContributionType.cs
@@ -1,7 +1,11 @@
-﻿namespace GitFyle.Core.Api.Brokers.Storages
+﻿using GitFyle.Core.Api.Models.Foundations.ContributionTypes;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitFyle.Core.Api.Brokers.Storages
 {
     internal partial interface IStorageBroker
     {
-
+        ValueTask<ContributionType> DeleteContributionTypeAsync(ContributionType contributionType);
     }
 }
