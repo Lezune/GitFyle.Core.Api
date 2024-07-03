82ebbf1480cc3eff4e2ce2c3366dcb4b51766fe6
Lezune <jakeleque@gmail.com>
2024-07-03 01:13:38 -0700

    Broker: SelectAllContributionTypesAsync - Method Added to StorageBroker.

--- GitFyle.Core.Api/Brokers/Storages/StorageBroker.ContributionTypes.cs
+++ GitFyle.Core.Api/Brokers/Storages/StorageBroker.ContributionTypes.cs
@@ -4,11 +4,16 @@
 
 using GitFyle.Core.Api.Models.Foundations.ContributionTypes;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace GitFyle.Core.Api.Brokers.Storages
 {
     internal partial class StorageBroker
     {
         public DbSet<ContributionType> ContributionTypes { get; set; }
+
+        public IQueryable<ContributionType> SelectAllContributionTypesAsync() =>
+            SelectAll<ContributionType>();
     }
 }
