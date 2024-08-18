d625881e3a57db416a347c0b848635b9ef6d13dd
Lezune <jakeleque@gmail.com>
2024-08-18 05:12:35 -0700

    Moved files from other branch.

--- GitFyle.Core.Api/Services/Foundations/Repositories/RepositoryService.cs
+++ GitFyle.Core.Api/Services/Foundations/Repositories/RepositoryService.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using GitFyle.Core.Api.Brokers.DateTimes;
+using GitFyle.Core.Api.Brokers.Loggings;
+using GitFyle.Core.Api.Brokers.Storages;
 using GitFyle.Core.Api.Models.Foundations.Repositories;
-
-
+using System.Threading.Tasks;
+using System;
 
 namespace GitFyle.Core.Api.Services.Foundations.Repositories
 {
-    internal partial class RepositoryService
+    internal partial class RepositoryService : IRepositoryService
     {
+        private readonly IStorageBroker storageBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
+        
+        public RepositoryService(
+            IStorageBroker storageBroker,
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
         public async ValueTask<Repository> AddAsync(Repository repository)
         {
             throw new NotImplementedException();
         }
-
     }
 }
