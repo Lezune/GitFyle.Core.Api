cb4411ed42043d07ae1fa736578d84021fa954fe
Lezune <jakeleque@gmail.com>
2024-08-18 04:23:07 -0700

    Declared AddAsync method. Currently not implemented.

--- GitFyle.Core.Api/Services/Foundations/Repositories/IRepositoryService.cs
+++ GitFyle.Core.Api/Services/Foundations/Repositories/IRepositoryService.cs
@@ -1,6 +1,11 @@
-﻿namespace GitFyle.Core.Api.Services.Foundations.Repositories
+﻿using GitFyle.Core.Api.Models.Foundations.Repositories;
+using System.Threading.Tasks;
+
+namespace GitFyle.Core.Api.Services.Foundations.Repositories
 {
     public class IRepositoryService
     {
+
+        ValueTask<Repository> AddAsync(Repository repository);
     }
 }
--- GitFyle.Core.Api/Services/Foundations/Repositories/RepositoryService.cs
+++ GitFyle.Core.Api/Services/Foundations/Repositories/RepositoryService.cs
@@ -1,6 +1,17 @@
-﻿namespace GitFyle.Core.Api.Services.Foundations.Repositories
+﻿using System;
+using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Repositories;
+
+
+
+namespace GitFyle.Core.Api.Services.Foundations.Repositories
 {
     public class RepositoryService
     {
+        public async ValueTask<Repository> AddAsync(Repository repository)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
